006ab2331a6accd276a13e6c28b87764048eaab8
md rafiqul islam <rafiqulislam11002@gmail.com>
2022-11-10 02:46:30 +0600

    jwt functionality added

--- index.js
+++ index.js
@@ -26,10 +26,34 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized Access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Access forbidden' });
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
+
 async function run() {
     try {
         const serviceCollection = client.db('bandaid-dental').collection('services');
         const reviewsCollection = client.db('bandaid-reviews').collection('reviews');
+
+        // jwt token 
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10h' });
+            res.send({ token });
+        });
+
 
         app.get('/services', async (req, res) => {
             const query = {};
@@ -64,17 +88,31 @@ async function run() {
             res.send(result);
         });
 
-        app.get('/my-reviews', async (req, res) => {
+        app.get('/my-review', async (req, res) => {
             let query = {};
-            // console.log(query.reviewId);
             if (req.query.reviewId) {
                 query = {
                     reviewId: req.query.reviewId
-
                 };
                 const cursor = reviewsCollection.find(query);
                 const result = await cursor.toArray();
                 return res.send(result);
+            }
+            const cursor = reviewsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        app.get('/my-reviews', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
+            if (decoded.email !== req.query.email) {
+                res.status(401).send({ message: 'Unauthorized Access' });
+            }
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                };
             }
             const cursor = reviewsCollection.find(query);
             const result = await cursor.toArray();
