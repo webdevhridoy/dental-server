df9ce94c99f8d6f30d4ae44611634936021c890a
md rafiqul islam <rafiqulislam11002@gmail.com>
2022-11-10 03:33:17 +0600

    sorting done

--- index.js
+++ index.js
@@ -57,13 +57,13 @@ async function run() {
 
         app.get('/services', async (req, res) => {
             const query = {};
-            const cursor = serviceCollection.find(query);
+            const cursor = serviceCollection.find(query).sort({ time: 1, time: -1 });;
             const services = await cursor.toArray();
             res.send(services);
         });
         app.get('/home-service', async (req, res) => {
             const query = {};
-            const cursor = serviceCollection.find(query);
+            const cursor = serviceCollection.find(query).sort({ time: 1, time: -1 });
             const services = await cursor.limit(3).toArray();
             res.send(services);
         });
@@ -94,7 +94,7 @@ async function run() {
                 query = {
                     reviewId: req.query.reviewId
                 };
-                const cursor = reviewsCollection.find(query);
+                const cursor = reviewsCollection.find(query).sort({ time: -1, time: 1 });
                 const result = await cursor.toArray();
                 return res.send(result);
             }
